2206bcd02926a7c827ccf29c17a21474e1e7a0ff
Anuj Datar <anuj.datar@gmail.com>
2020-08-16 18:38:32 +0530

    style testing

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <h4>Nuxt/Vue Components</h4>
+    <!-- <h4 class="text-main">Nuxt/Vue Components</h4> -->
   </div>
 </template>
 
@@ -8,5 +9,9 @@
 export default {}
 </script>
 
-<style>
+<style lang="scss">
+// @import "../assets/scss/main";
+// h4 {
+//   color: $test;
+// }
 </style>
