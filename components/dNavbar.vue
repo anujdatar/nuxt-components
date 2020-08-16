d89e2158fb33d554b008eb3137940108502e6f4a
Anuj Datar <anuj.datar@gmail.com>
2020-08-16 18:37:16 +0530

    top navbar initial

---
+++ components/dNavbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav>
+    <div class="brand">
+      <nuxt-link to="/">
+        {{ appTitle }}
+      </nuxt-link>
+    </div>
+    <div class="nav-links">
+      <ul>
+        <li
+          v-for="item in routes"
+          :key="item.title"
+        >
+          <nuxt-link
+            :to="item.path"
+          >
+            {{ item.title }}
+          </nuxt-link>
+        </li>
+      </ul>
+    </div>
+
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    routes: {
+      type: Array,
+      required: true
+    },
+    appTitle: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
