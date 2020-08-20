e14b8cd5b729a5c08cf36ce3239b59036cd77752
Anuj Datar <anuj.datar@gmail.com>
2020-08-20 21:17:36 +0530

    added navbar styling

--- components/dNavbar.vue
+++ components/dNavbar.vue
@@ -19,7 +19,6 @@
         </li>
       </ul>
     </div>
-
   </nav>
 </template>
 
@@ -37,3 +36,104 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+nav {
+  display: flex;
+  width: 100%;
+  @include theme-aware-alpha('background-color', 'primary', 0.1);
+  // @include theme-aware('background-color', 'bg');
+  &.sticky {
+    position: sticky;
+    top: 0;
+  }
+  &.elevate-on-scroll {
+    background-color: transparent;
+    position: sticky;
+    top: 0;
+    &.is-scrolled {
+      // @include theme-aware-alpha('background', 'bg-main', 0.9);
+      box-shadow: 0 1px rgba(#7b7b7b, .5);
+    }
+  }
+}
+nav .brand {
+  font-size: 32px;
+  font-weight: bold;
+  padding: 10px;
+  letter-spacing: 3px;
+  a {
+    text-decoration: none;
+  }
+}
+nav .nav-links {
+  display: flex;
+  &.right {
+    margin-left: auto;
+  }
+  ul {
+    display: flex;
+    justify-content: space-around;
+    list-style: none;
+    li {
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      &:hover {
+        @include theme-aware('background', 'bg-contrast');
+        filter: brightness(75%);
+      }
+    }
+    a {
+      color: white;
+      text-decoration: none;
+      font-size: 14px;
+      font-weight: 500;
+      padding: 0 15px;
+      text-transform: uppercase;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      &.nuxt-link-exact-active {
+        // @include theme-aware-important('color', 'warning');
+        // @include theme-aware-alpha('background-color', 'warning', 0.3);
+        color: color('deep-orange');
+        // background-color: color('orange');
+      }
+    }
+  }
+}
+nav .burger {
+  display: none;
+  align-self: center;
+  cursor: pointer;
+  padding: 10px;
+  div {
+    // @include theme-aware('background-color', 'text-contrast');
+    background-color: red;
+    width: 25px;
+    height: 2px;
+    margin: 5px;
+    transition: all 0.3s ease;
+  }
+  &.toggle .line1 {
+    transform: rotate(-45deg) translate(-5px,5px);
+  }
+  &.toggle .line2 {
+      opacity: 0;
+  }
+  &.toggle .line3 {
+      transform: rotate(45deg) translate(-5px,-5px);
+  }
+}
+
+@media screen and (max-width:768px) {
+  nav .nav-links {
+    display: none;
+  }
+  nav .burger {
+    display: block;
+    margin-left: auto;
+  }
+}
+</style>
