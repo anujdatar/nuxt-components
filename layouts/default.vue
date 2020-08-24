ec9b53d664268d1b8de102fa02f5e4f03aab1dac
Anuj Datar <anuj.datar@gmail.com>
2020-08-24 17:06:30 +0530

    added dummy text to test scroll effects

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
-    <d-navbar :routes="routes" :app-title="appTitle" />
+    <d-navbar
+      :routes="routes"
+      :app-title="appTitle"
+      elevate-on-scroll
+      right
+    />
     <Nuxt />
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur, corrupti.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus, hic nemo, non, cum ullam veritatis architecto dolorem velit adipisci eligendi nisi expedita facere temporibus quibusdam veniam iste ad. Ipsum error quis recusandae, eveniet minus quam necessitatibus libero ex harum accusamus aut, voluptate mollitia, reiciendis eos vero porro assumenda. Ratione, praesentium!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus impedit ab nisi nemo asperiores quam commodi, eaque deserunt tempora dolor.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque veritatis ea aliquam magnam natus ipsum quaerat ut corporis odit! Cumque illum provident mollitia labore, fugiat doloribus facere quis error odio repellat nisi fuga eligendi laborum quibusdam ad! Quasi maxime, laborum labore facilis, nobis at debitis incidunt omnis, nemo sed suscipit.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus iste natus ratione exercitationem impedit neque, nostrum cupiditate dolor delectus, quia aliquam facere commodi vitae recusandae laboriosam provident voluptas nesciunt dicta. Necessitatibus numquam magnam a porro fugit earum repellat voluptatibus quos odio impedit, consectetur ipsum deserunt iure nemo quam similique esse odit! Eos fuga maiores eius vel recusandae quod aliquid nihil obcaecati autem, voluptatibus quibusdam odit a tenetur reprehenderit, ea porro hic, aliquam architecto! Quam eaque dignissimos ratione reiciendis dolorem pariatur aspernatur repellendus! A quod eligendi dignissimos optio dolorem, enim reiciendis ea incidunt laudantium numquam ratione atque perferendis natus, temporibus maxime?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus iste natus ratione exercitationem impedit neque, nostrum cupiditate dolor delectus, quia aliquam facere commodi vitae recusandae laboriosam provident voluptas nesciunt dicta. Necessitatibus numquam magnam a porro fugit earum repellat voluptatibus quos odio impedit, consectetur ipsum deserunt iure nemo quam similique esse odit! Eos fuga maiores eius vel recusandae quod aliquid nihil obcaecati autem, voluptatibus quibusdam odit a tenetur reprehenderit, ea porro hic, aliquam architecto! Quam eaque dignissimos ratione reiciendis dolorem pariatur aspernatur repellendus! A quod eligendi dignissimos optio dolorem, enim reiciendis ea incidunt laudantium numquam ratione atque perferendis natus, temporibus maxime?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus iste natus ratione exercitationem impedit neque, nostrum cupiditate dolor delectus, quia aliquam facere commodi vitae recusandae laboriosam provident voluptas nesciunt dicta. Necessitatibus numquam magnam a porro fugit earum repellat voluptatibus quos odio impedit, consectetur ipsum deserunt iure nemo quam similique esse odit! Eos fuga maiores eius vel recusandae quod aliquid nihil obcaecati autem, voluptatibus quibusdam odit a tenetur reprehenderit, ea porro hic, aliquam architecto! Quam eaque dignissimos ratione reiciendis dolorem pariatur aspernatur repellendus! A quod eligendi dignissimos optio dolorem, enim reiciendis ea incidunt laudantium numquam ratione atque perferendis natus, temporibus maxime?</p>
   </div>
 </template>
 
