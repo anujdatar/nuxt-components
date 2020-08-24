18d8de24c1399b145fc1c02822991e63eebba943
Anuj Datar <anuj.datar@gmail.com>
2020-08-24 17:06:58 +0530

    added home to navbar nav-list

--- layouts/default.vue
+++ layouts/default.vue
@@ -23,6 +23,7 @@ export default {
     return {
       appTitle: 'Nuxt/Vue Components',
       routes: [
+        { title: 'Home', path: '/' },
         { title: 'About', path: '/about' },
         { title: 'Contact', path: '/contact' }
       ]
