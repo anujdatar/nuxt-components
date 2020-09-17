d6b760158797325680b7b5fb449889c5e38fb53b
Anuj Datar <anuj.datar@gmail.com>
2020-09-17 12:12:10 +0530

    added switches/buttons for testing,

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="container">
     <h2>Home Page</h2>
-    <d-switch />
+    <div class="test">
+      <d-switch />
+      <!-- <br> -->
+      <d-button text="Lorem" @click="toggleSidebar" />
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+import { mapMutations } from 'vuex'
+
+export default {
+  methods: {
+    ...mapMutations(['toggleSidebar']),
+    onClick () {
+      console.log('aaaaaaaaaaa')
+    }
+  }
+}
 </script>
 
 <style lang="scss">
+.test {
+  display: flex;
+}
 </style>
